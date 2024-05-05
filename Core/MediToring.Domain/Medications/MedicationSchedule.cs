3c5a67a9ad7faa5fc4a7dc4206b8684dee201256
AlexanderLukashuk <upiter465@gmail.com>
2024-05-05 16:40:20 +0500

    added user and medication in schedule class to create one-to-many relations between user and schedule and medication and schedule

--- Core/MediToring.Domain/Medications/MedicationSchedule.cs
+++ Core/MediToring.Domain/Medications/MedicationSchedule.cs
@@ -1,11 +1,14 @@
 using BuildingBlocks.Domain;
+using MediToring.Domain.Users;
 
 namespace MediToring.Domain.Medications;
 
 public class MedicationSchedule : EntityBase<Guid>
 {
     public int MedicationId { get; set; }
+    public Medication Medication { get; set; }
     public int UserId { get; set; }
+    public User User { get; set; }
     public DateTime StartTime { get; set; }
     public DateTime EndTime { get; set; }
     public ICollection<DailyDose> DailyDoses { get; set; }
