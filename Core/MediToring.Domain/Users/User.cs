041d3abd0cf2f4ba333c1b996e758d0c1288b042
AlexanderLukashuk <upiter465@gmail.com>
2024-05-05 15:02:25 +0500

    created user class

---
+++ Core/MediToring.Domain/Users/User.cs
@@ -0,0 +1,24 @@
+using BuildingBlocks.Domain;
+using MediToring.Domain.Enums;
+
+namespace MediToring.Domain.Users;
+
+public class User : EntityBase<Guid>
+{
+    public string Email { get; private set; }
+    public string Name { get; set; }
+    public string PasswordHash { get; private set; }
+    public Role UserRole { get; set; }
+
+    private User() { }
+
+    public User(string email, string passwordHash) : this(Guid.NewGuid(), email, passwordHash) { }
+
+    public User(Guid guid, string email, string passwordHash)
+    {
+        Id = guid;
+
+        Email = email;
+        PasswordHash = passwordHash;
+    }
+}
