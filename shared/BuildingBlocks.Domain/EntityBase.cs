293d7a118732041e51ab896383df88c30fff1436
AlexanderLukashuk <upiter465@gmail.com>
2024-05-05 15:04:50 +0500

    created shared folder with base entity for classes

---
+++ shared/BuildingBlocks.Domain/EntityBase.cs
@@ -0,0 +1,12 @@
+namespace BuildingBlocks.Domain;
+
+public abstract class EntityBase
+{
+    public int Id { get; set; }
+}
+
+public abstract class EntityBase<TId>
+    where TId : struct, IEquatable<TId>
+{
+    public TId Id { get; set; }
+}
