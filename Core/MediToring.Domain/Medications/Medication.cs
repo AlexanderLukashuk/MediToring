6be0911f4faf5539d94bc03884c5d65524ef4490
AlexanderLukashuk <upiter465@gmail.com>
2024-05-08 19:11:15 +0500

    created constructors in medication class

--- Core/MediToring.Domain/Medications/Medication.cs
+++ Core/MediToring.Domain/Medications/Medication.cs
@@ -8,4 +8,18 @@ public class Medication : EntityBase<Guid>
     public string Dosage { get; set; }
     public string Instruction { get; set; }
     public ICollection<MedicationSchedule> Schedules { get; set; }
+
+    private Medication() { }
+
+    public Medication(string name, string dosage, string instruction)
+        : this(Guid.NewGuid(), name, dosage, instruction) { }
+
+    public Medication(Guid guid, string name, string dosage, string instruction)
+    {
+        Id = guid;
+
+        Name = name;
+        Dosage = dosage;
+        Instruction = instruction;
+    }
 }
