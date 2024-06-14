23ce677b771aad4402b9590a43d5cfbaf0cb4f29
AlexanderLukashuk <upiter465@gmail.com>
2024-06-14 18:50:42 +0500

    create dto for DailyDose

---
+++ Core/MediToring.Application/Features/MedicationSchedules/Commands/Models/DailyDoseDto.cs
@@ -0,0 +1,9 @@
+using MediToring.Domain.Enums;
+
+namespace MediToring.Application.Features.MedicationSchedules.Commands.Models;
+
+public class DailyDoseDto
+{
+    public TimeOfDay TimeOfDay { get; set; }
+    public bool BeforeMeal { get; set; }
+}
