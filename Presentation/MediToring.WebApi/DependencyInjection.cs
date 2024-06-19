cc5f097a1bae1367c97d70ce0cbc6e94bd419604
AlexanderLukashuk <upiter465@gmail.com>
2024-06-20 03:01:57 +0500

    create DI for IdentityServer

---
+++ Presentation/MediToring.WebApi/DependencyInjection.cs
@@ -0,0 +1,39 @@
+using System.Text;
+using MediToring.Infrastructure;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+
+namespace MediToring.WebApi;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddIdentityAuth(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddIdentity<IdentityUser, IdentityRole>()
+            .AddEntityFrameworkStores<MediToringDbContext>()
+            .AddDefaultTokenProviders();
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            options.SaveToken = true;
+            options.RequireHttpsMetadata = false;
+            options.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidAudience = configuration["JWT:ValidAudience"],
+                ValidIssuer = configuration["JWT:ValidIssuer"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]))
+            };
+        });
+
+        return services;
+    }
+}
