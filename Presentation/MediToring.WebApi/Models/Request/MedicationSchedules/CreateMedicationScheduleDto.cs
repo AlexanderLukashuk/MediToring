1338df802d70a57fb0c136cb1c17681f2d5a5af9
AlexanderLukashuk <upiter465@gmail.com>
2024-06-22 19:21:06 +0500

    add mapping realization

--- Presentation/MediToring.WebApi/Models/Request/MedicationSchedules/CreateMedicationScheduleDto.cs
+++ Presentation/MediToring.WebApi/Models/Request/MedicationSchedules/CreateMedicationScheduleDto.cs
@@ -1,10 +1,14 @@
 namespace MediToring.WebApi.Models.Request.MedicationSchedules;
 
-public class CreateMedicationScheduleDto
+public class CreateMedicationScheduleDto : IMapWith<CreateScheduleCommand>
 {
     public Guid MedicationId { get; set; }
-    public Guid UserId { get; set; }
     public DateTime StartTime { get; set; }
     public DateTime EndTime { get; set; }
     public ICollection<DailyDoseDto> DailyDoses { get; set; }
+
+    public void Mapping(Profile profile)
+    {
+        profile.CreateMap<CreateMedicationScheduleDto, CreateScheduleCommand>();
+    }
 }
--- Presentation/MediToring.WebApi/Models/Request/MedicationSchedules/UpdateMedicationScheduleDto.cs
+++ Presentation/MediToring.WebApi/Models/Request/MedicationSchedules/UpdateMedicationScheduleDto.cs
@@ -1,11 +1,14 @@
 namespace MediToring.WebApi.Models.Request.MedicationSchedules;
 
-public class UpdateMedicationScheduleDto
+public class UpdateMedicationScheduleDto : IMapWith<UpdateScheduleCommand>
 {
-    public Guid Id { get; set; }
     public Guid MedicationId { get; set; }
-    public Guid UserId { get; set; }
     public DateTime StartTime { get; set; }
     public DateTime EndTime { get; set; }
     public ICollection<DailyDoseDto> DailyDoses { get; set; }
+
+    public void Mapping(Profile profile)
+    {
+        profile.CreateMap<UpdateMedicationScheduleDto, UpdateScheduleCommand>();
+    }
 }
