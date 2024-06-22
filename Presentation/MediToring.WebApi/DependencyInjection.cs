b087205b3b2221e11e2f78996bae999680cd776b
AlexanderLukashuk <upiter465@gmail.com>
2024-06-22 19:33:08 +0500

    refactor DI

--- Presentation/MediToring.WebApi/DependencyInjection.cs
+++ Presentation/MediToring.WebApi/DependencyInjection.cs
@@ -1,5 +1,3 @@
-using Microsoft.OpenApi.Models;
-
 namespace MediToring.WebApi;
 
 public static class DependencyInjection
@@ -33,42 +31,8 @@ public static class DependencyInjection
         });
 
         services.AddEndpointsApiExplorer();
-        services.AddSwaggerGen(s =>
-        {
-            s.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi", Version = "v1" });
-
-            s.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-            {
-                Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer'  [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 123445abcdef\"",
-                Name = "Authorization",
-                In = ParameterLocation.Header,
-                Type = SecuritySchemeType.ApiKey,
-                Scheme = "Bearer"
-            });
-
-            s.AddSecurityRequirement(new OpenApiSecurityRequirement()
-            {
-                {
-                    new OpenApiSecurityScheme
-                    {
-                        Reference = new OpenApiReference
-                        {
-                            Type = ReferenceType.SecurityScheme,
-                            Id = "Bearer"
-                        },
-                        Scheme = "oauth2",
-                        Name = "Bearer",
-                        In = ParameterLocation.Header,
-                    },
-                    new List<string>()
-                }
-            });
-        });
-
-        services.AddAuthorization(options =>
-        {
-            options.AddPolicy("UserPolicy", policy => policy.RequireClaim(ClaimTypes.NameIdentifier));
-        });
+        
+        ConfigureSwagger(services);
 
         return services;
     }
@@ -99,6 +63,46 @@ public static class DependencyInjection
                 NameClaimType = ClaimTypes.Name,
                 RoleClaimType = ClaimTypes.Role,
             };
+        });
+
+        services.AddAuthorization(options =>
+        {
+            options.AddPolicy("UserPolicy", policy => policy.RequireClaim(ClaimTypes.NameIdentifier));
+        });
+    }
+
+    private static void ConfigureSwagger(IServiceCollection services)
+    {
+        services.AddSwaggerGen(s =>
+        {
+            s.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi", Version = "v1" });
+
+            s.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer'  [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 123445abcdef\"",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer"
+            });
+
+            s.AddSecurityRequirement(new OpenApiSecurityRequirement()
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        },
+                        Scheme = "oauth2",
+                        Name = "Bearer",
+                        In = ParameterLocation.Header,
+                    },
+                    new List<string>()
+                }
+            });
         });
     }
 }
