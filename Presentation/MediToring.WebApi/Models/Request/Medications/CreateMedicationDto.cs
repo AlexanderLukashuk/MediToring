cdda0f64b84063543c9b906c20c527867ad20027
AlexanderLukashuk <upiter465@gmail.com>
2024-06-22 12:30:08 +0500

    add mapping in cqrs create and delete dto

--- Presentation/MediToring.WebApi/Models/Request/Medications/CreateMedicationDto.cs
+++ Presentation/MediToring.WebApi/Models/Request/Medications/CreateMedicationDto.cs
@@ -1,8 +1,13 @@
 namespace MediToring.WebApi.Models.Request.Medications;
 
-public class CreateMedicationDto
+public class CreateMedicationDto : IMapWith<CraeteMedicationCommand>
 {
     public string Name { get; set; }
     public string Dosage { get; set; }
     public string Instruction { get; set; }
+
+    public void Mapping(Profile profile)
+    {
+        profile.CreateMap<CreateMedicationDto, CraeteMedicationCommand>();
+    }
 }
--- Presentation/MediToring.WebApi/Models/Request/Medications/UpdateMedicationDto.cs
+++ Presentation/MediToring.WebApi/Models/Request/Medications/UpdateMedicationDto.cs
@@ -1,8 +1,13 @@
 namespace MediToring.WebApi.Models.Request.Medications;
 
-public class UpdateMedicationDto
+public class UpdateMedicationDto : IMapWith<UpdateMedicationCommand>
 {
     public string Name { get; set; }
     public string Dosage { get; set; }
     public string Instruction { get; set; }
+
+    public void Mapping(Profile profile)
+    {
+        profile.CreateMap<UpdateMedicationDto, UpdateMedicationCommand>();
+    }
 }
