805d663eb1ccc903796ff60e9f53ebfa1ef246a9
AlexanderLukashuk <upiter465@gmail.com>
2024-07-07 06:08:22 +0500

    create user roles

---
+++ Presentation/MediToring.WebApi/Models/UserRoles.cs
@@ -0,0 +1,8 @@
+namespace MediToring.WebApi.Models;
+
+public class UserRoles
+{
+    public const string Admin = "Admin";
+    public const string Doctor = "Doctor";
+    public const string User = "User";
+}
