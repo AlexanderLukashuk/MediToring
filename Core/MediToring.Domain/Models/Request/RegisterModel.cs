6ec87b179e9bf488f9b750a4920d318ac8c4d6ed
AlexanderLukashuk <upiter465@gmail.com>
2024-07-09 23:21:01 +0500

    add properties for user profile

--- Core/MediToring.Domain/Models/Request/RegisterModel.cs
+++ Core/MediToring.Domain/Models/Request/RegisterModel.cs
@@ -12,4 +12,17 @@ public class RegisterModel
     [Required(ErrorMessage = "Password is required")]
     [MinLength(8, ErrorMessage = "Password must be at least 8 characters long")]
     public required string Password { get; set; }
+
+    [Required(ErrorMessage = "First Name is required")]
+    public required string FirstName { get; set; }
+
+    [Required(ErrorMessage = "Last Name is required")]
+    public required string LastName { get; set; }
+
+    [Required(ErrorMessage = "Date of Birth is required")]
+    public DateTime DateOfBirth { get; set; }
+
+    public double Height { get; set; }
+
+    public double Weight { get; set; }
 }
