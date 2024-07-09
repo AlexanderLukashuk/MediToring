b14a01a2cd2082cc14f82f596d50bc6a74c2f55b
AlexanderLukashuk <upiter465@gmail.com>
2024-07-09 23:23:37 +0500

    update datetime converter

--- Core/MediToring.Application/Common/Helpers/DateTimeConverter.cs
+++ Core/MediToring.Application/Common/Helpers/DateTimeConverter.cs
@@ -2,10 +2,15 @@ namespace MediToring.Application.Common.Helpers;
 
 public static class DateTimeConverter
 {
+    public static DateTime ConvertToUtc(DateTime dateTime)
+    {
+        return DateTime.SpecifyKind(dateTime, DateTimeKind.Utc);
+    }
+
     public static (DateTime StartTimeUtc, DateTime EndTimeUtc) ConvertToUtc(DateTime startTime, DateTime endTime)
     {
-        var startTimeUtc = DateTime.SpecifyKind(startTime, DateTimeKind.Utc);
-        var endTimeUtc = DateTime.SpecifyKind(endTime, DateTimeKind.Utc);
+        var startTimeUtc = ConvertToUtc(startTime);
+        var endTimeUtc = ConvertToUtc(endTime);
         return (startTimeUtc, endTimeUtc);
     }
 }
