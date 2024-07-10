0dfc927d96b380ad9a5977f01ecf5806b685b0ea
AlexanderLukashuk <upiter465@gmail.com>
2024-07-10 20:52:36 +0500

    add method to register admin

--- Presentation/MediToring.WebApi/Controllers/AuthenticateController.cs
+++ Presentation/MediToring.WebApi/Controllers/AuthenticateController.cs
@@ -1,5 +1,6 @@
 using MediToring.Application.Features.Profiles.Commands.CreateProfile;
 using MediToring.Domain.Models;
+using MediToring.WebApi.Models;
 using MediToring.WebApi.Models.Request.Profiles;
 
 namespace MediToring.WebApi.Controllers;
@@ -92,6 +93,36 @@ public class AuthenticateController(
             return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = " Error", Message = "Create profile was failed" });
 
         return Ok(new Response { Status = "Success", Message = "User created successfully!" });
+    }
+
+    [HttpPost]
+    [Route("register-admin")]
+    public async Task<IActionResult> RegisterAdmin([FromBody] RegisterModel model)
+    {
+        var userExists = await userManager.FindByNameAsync(model.Username);
+        if (userExists != null)
+            return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User already exists!" });
+        
+        IdentityUser user = new()
+        {
+            Email = model.Email,
+            SecurityStamp = Guid.NewGuid().ToString(),
+            UserName = model.Username
+        };
+
+        var result = await userManager.CreateAsync(user, model.Password);
+        if (!result.Succeeded)
+            return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User creation failed! Please check user details and try again." });
+        
+        if (!await roleManager.RoleExistsAsync(UserRoles.Admin))
+            await roleManager.CreateAsync(new IdentityRole(UserRoles.Admin));
+        
+        if (await roleManager.RoleExistsAsync(UserRoles.Admin))
+        {
+            await userManager.AddToRoleAsync(user, UserRoles.Admin);
+        }
+
+        return Ok(new Response { Status = "Success", Message = "Admin created successfully!" });
     }
 
     private JwtSecurityToken GetToken(List<Claim> authClaims)
