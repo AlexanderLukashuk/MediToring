1e3798ea4f3ff70416155d98781964b3c13f89b4
AlexanderLukashuk <upiter465@gmail.com>
2024-07-17 21:35:39 +0500

    add routes for doctor's profiles

--- Presentation/MediToring.Frontend/src/app/app.routes.ts
+++ Presentation/MediToring.Frontend/src/app/app.routes.ts
@@ -1,10 +1,13 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { MedicationListComponent } from './medication/medication-list/medication-list.component';
 import { MedicationDetailComponent } from './medication/medication-detail/medication-detail.component';
 import { MedicationScheduleListComponent } from './medication-schedule/schedule-list/schedule-list.component';
 import { MedicationScheduleDetailComponent } from './medication-schedule/schedule-detail/schedule-detail.component';
+import { DoctorListComponent } from './doctor-profile/doctor-list/doctor-list.component';
+import { DoctorDetailComponent } from './doctor-profile/doctor-detail/doctor-detail.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
   { path: 'auth/login', component: LoginComponent },
@@ -13,5 +16,13 @@ export const routes: Routes = [
   { path: 'medication/:id', component: MedicationDetailComponent },
   { path: 'medication-schedule', component: MedicationScheduleListComponent },
   { path: 'medication-schedule/:id', component: MedicationScheduleDetailComponent },
+  { path: 'doctor', component: DoctorListComponent },
+  { path: 'doctor/:id', component: DoctorDetailComponent },
   { path: '', redirectTo: '/auth/login', pathMatch: 'full' }
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
