ff738e73bd6a7ca311d74869cdf7705feedfef54
AlexanderLukashuk <upiter465@gmail.com>
2024-07-17 21:27:55 +0500

    add new migrations for doctor's profiles

---
+++ Infrastructure/MediToring.Infrastructure/Migrations/20240717115337_AddDoctorProfile.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediToring.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDoctorProfile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DoctorApplications",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    Specialization = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Qualification = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    ExperienceYears = table.Column<int>(type: "integer", nullable: false),
+                    Clinic = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    Bio = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: true),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    SubmittedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DoctorApplications", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DoctorProfiles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserProfileId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Specialization = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Qualification = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    ExperienceYears = table.Column<int>(type: "integer", nullable: false),
+                    Clinic = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    Bio = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: true),
+                    Reviews = table.Column<string>(type: "text", nullable: false),
+                    Rating = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DoctorProfiles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DoctorProfiles_UserProfiles_UserProfileId",
+                        column: x => x.UserProfileId,
+                        principalTable: "UserProfiles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DoctorProfiles_UserProfileId",
+                table: "DoctorProfiles",
+                column: "UserProfileId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DoctorApplications");
+
+            migrationBuilder.DropTable(
+                name: "DoctorProfiles");
+        }
+    }
+}
