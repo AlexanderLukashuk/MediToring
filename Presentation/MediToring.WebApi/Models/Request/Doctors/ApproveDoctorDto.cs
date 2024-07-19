ed17a52d1d7cbc23de2a0952bdf1df32b45f637d
AlexanderLukashuk <upiter465@gmail.com>
2024-07-19 16:56:49 +0500

    update dto model

--- Presentation/MediToring.WebApi/Models/Request/Doctors/ApproveDoctorDto.cs
+++ Presentation/MediToring.WebApi/Models/Request/Doctors/ApproveDoctorDto.cs
@@ -2,11 +2,12 @@ namespace MediToring.WebApi.Models.Request.Doctors;
 
 public class ApproveDoctorDto
 {
-    public string? UserId { get; set; }
-    public Guid UserProfileId { get; set; }
-    public string? Specialization { get; set; }
-    public string? Qualification { get; set; }
-    public int ExperienceYears { get; set; }
-    public string? Clinic { get; set; }
-    public string? Bio { get; set; }
+    // public string? UserId { get; set; }
+    // public Guid UserProfileId { get; set; }
+    // public string? Specialization { get; set; }
+    // public string? Qualification { get; set; }
+    // public int ExperienceYears { get; set; }
+    // public string? Clinic { get; set; }
+    // public string? Bio { get; set; }
+    public Guid ApplicationId { get; set; }
 }
