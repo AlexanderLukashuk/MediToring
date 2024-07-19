9f14d3dadd8af6ef6d458a5b50b20752abecd251
AlexanderLukashuk <upiter465@gmail.com>
2024-07-19 16:55:12 +0500

    update approve method and add method to get all applications

--- Presentation/MediToring.WebApi/Controllers/AdminController.cs
+++ Presentation/MediToring.WebApi/Controllers/AdminController.cs
@@ -1,4 +1,6 @@
+using MediToring.Application.Features.Profiles.Commands.ApproveDoctor;
 using MediToring.Application.Features.Profiles.Commands.CreateDoctorProfile;
+using MediToring.Application.Features.Profiles.Queries.GetAllDoctorsApplications;
 using MediToring.WebApi.Models;
 using MediToring.WebApi.Models.Request.Doctors;
 
@@ -7,54 +9,24 @@ namespace MediToring.WebApi.Controllers;
 [Authorize(Policy = "UserPolicy")]
 [ApiController]
 [Route("api/[controller]")]
-public class AdminController(
-    UserManager<IdentityUser> userManager, 
-    RoleManager<IdentityRole> roleManager, 
-    IMediator mediator) 
-    : ControllerBase
+public class AdminController(IMediator mediator) : ControllerBase
 {
     [HttpPost]
     [Authorize(Roles = "Admin")]
     [Route("approve-doctor")]
     public async Task<IActionResult> ApproveDoctor([FromBody] ApproveDoctorDto model)
     {
-        if (string.IsNullOrEmpty(model.UserId))
-        {
-            // правильно ли тут возвращать 500 ошибку?????
-            return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User id can't be null or empty" });
-        }
-
-        var user = await userManager.FindByIdAsync(model.UserId); // мб переделать позже на поиск по имени
-        if (user == null)
-        {
-            return NotFound(new Response { Status = "Error", Message = "User not found" });
-        }
-
-        if (!await roleManager.RoleExistsAsync(UserRoles.Doctor))
-            await roleManager.CreateAsync(new IdentityRole(UserRoles.Doctor));
-        
-        if (await roleManager.RoleExistsAsync(UserRoles.Doctor))
-        {
-            await userManager.AddToRoleAsync(user, UserRoles.Doctor);
-        }
-
-        // var result = await userManager.AddToRoleAsync(user, UserRoles.Doctor);
-        // if (!result.Succeeded)
-        // {
-        //     return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "Failed to assign Doctor role" });
-        // }
-
-        var command = new CreateDoctorProfileCommand
-        {
-            UserProfileId = model.UserProfileId,
-            Specialization = model.Specialization,
-            Qualification = model.Qualification,
-            ExperienceYears = model.ExperienceYears,
-            Clinic = model.Clinic,
-            Bio = model.Bio
-        };
-
+        var command = new ApproveDoctorCommand { ApplicationId = model.ApplicationId };
         await mediator.Send(command);
-        return Ok(new Response { Status = "Succes", Message = "Doctor approved successfully!" });
+        return Ok(new { Status = "Success", Message = "Doctor approved successfully!" });
+    }
+
+    [HttpGet]
+    [Authorize(Roles = "Admin")]
+    [Route("applications")]
+    public async Task<IActionResult> GetAllApplications()
+    {
+        var applications = await mediator.Send(new GetDoctorApplicationsQuery());
+        return Ok(applications);
     }
 }
